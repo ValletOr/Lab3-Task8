cf812c476005b136fb275c0bf0480b0a2d01ad69
ValletOr <mtvac11@gmail.com>
2021-10-16 18:40:24 +0800

    Добавьте файлы проекта.

---
+++ Lab3 Task8/MathSet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task8
+{
+    public class MathSet
+    {
+        //Поля
+        private int[] set;
+
+        //Конструкторы
+        public MathSet(string input)
+        {
+            this.set = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        }
+        public MathSet(int[] input)
+        {
+            this.set = input;
+        }
+
+        //Методы
+
+        /// <summary>
+        /// Возвращает содержимое объекта.
+        /// </summary>
+        /// <returns></returns>
+        public string Verbose()
+        {
+            string outMessage = "";
+            foreach (int i in this.set)
+            {
+                outMessage += i + " ";
+            }
+            return outMessage;
+        }
+
+        public static MathSet operator+(MathSet firstSet, MathSet secondSet)
+        {
+            string form = "";
+            for (int i = 0; i < firstSet.set.Length; i++)
+            {
+                form += firstSet.set[i] + " ";
+
+            }
+            return outMathSet;
+        }
+    }
+}
---
+++ Lab3 Task8Tests/MathSetTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3_Task8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task8.Tests
+{
+    [TestClass()]
+    public class MathSetTests
+    {
+        [TestMethod()]
+        public void First()
+        {
+            string a = "1 2 3 4 5 6 7 8 9 10";
+            MathSet b = new MathSet(a);
+            Assert.AreEqual("1 2 3 4 5 6 7 8 9 10 ", b.Verbose());
+        }
+    }
+}
